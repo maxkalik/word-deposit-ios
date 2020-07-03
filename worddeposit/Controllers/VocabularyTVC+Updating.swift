3b46674bed8dad7d7d2ae402477ec6a837125d97
Maksim Kalik <maxkalik@gmail.com>
2020-07-03 18:11:23 +0300

    updating view table after filtering

--- worddeposit/Controllers/VocabularyTVC+Updating.swift
+++ worddeposit/Controllers/VocabularyTVC+Updating.swift
@@ -1,10 +1,31 @@
 import UIKit
 
 extension VocabularyTVC: UISearchResultsUpdating {
+    
+    private func normalizeString(str: String) -> String {
+        let pattern = "[^A-Za-z0-9]+"
+        let result = str.replacingOccurrences(of: pattern, with: "", options: [.regularExpression])
+        return result
+    }
+    
+   
     func updateSearchResults(for searchController: UISearchController) {
-        let searchResults = words
+        let whitespaceCharacterSet = CharacterSet.whitespaces
+        let strippedString = searchController.searchBar.text!.trimmingCharacters(in: whitespaceCharacterSet)
         
+        let preparedString = normalizeString(str: strippedString)
         
+        let searchResults = words.filter { (word) -> Bool in
+            word.example.range(of: preparedString) != nil ||
+            word.translation.range(of: preparedString) != nil
+        }
+        
+        if let resultsController = searchController.searchResultsController as? VocabularyResultsTVC {
+            resultsController.filteredWords = searchResults
+            resultsController.tableView.reloadData()
+            
+            resultsController.resultsLabel.text = resultsController.filteredWords.isEmpty ? "No found" : "Items found: \(resultsController.filteredWords.count)"
+        }
         
     }
 }
