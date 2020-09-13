764fe9f7043198787c0fce4be161bea684291325
maxkalik <maxkalik@gmail.com>
2020-09-14 01:03:35 +0300

    image picker with status bar

--- worddeposit/Utils/Configurations.swift
+++ worddeposit/Utils/Configurations.swift
@@ -14,6 +14,7 @@ class YPImagePickerConfig {
         config.screens = [.library, .photo]
         config.albumName = "WordDeposit"
         config.showsPhotoFilters = false
+        config.hidesStatusBar = false
         
         let newCapturePhotoImage = UIImage.circle(diameter: 80.00, color: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1))
         config.icons.capturePhotoImage = newCapturePhotoImage
