6b0cf2b50657ba8d65a7cbf0a97af3f048c0f97c
maxkalik <maxkalik@gmail.com>
2020-09-30 21:04:48 +0300

    nav controller nav bar background without shadow

--- worddeposit/Extensions/UINavigationController.swift
+++ worddeposit/Extensions/UINavigationController.swift
@@ -5,11 +5,21 @@ extension UINavigationController {
     open override func viewDidLoad() {
         super.viewDidLoad()
         setFont()
+        setUpNavBar()
     }
     
     func setFont() {
-        self.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: Fonts.bold, size: 22)!, NSAttributedString.Key.kern: -0.8]
-        self.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont(name: Fonts.bold, size: 36)!, NSAttributedString.Key.kern: -0.8]
-        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: Fonts.bold, size: 14)!, NSAttributedString.Key.kern: -0.5], for: UIControl.State.normal)
+        navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: Fonts.bold, size: 22)!, NSAttributedString.Key.kern: -0.8]
+        navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont(name: Fonts.bold, size: 36)!, NSAttributedString.Key.kern: -0.8]
+        navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: Fonts.bold, size: 14)!, NSAttributedString.Key.kern: -0.5]
+    }
+    
+    func setUpNavBar() {
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.shadowColor = .clear
+        navBarAppearance.shadowImage = UIImage()
+        navBarAppearance.backgroundColor = Colors.silver
+        navigationBar.standardAppearance = navBarAppearance
+        navigationBar.scrollEdgeAppearance = navBarAppearance
     }
 }
