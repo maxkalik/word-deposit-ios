d3fba448d0f830d45ffcdbccfe78b287159f8d8b
maxkalik <maxkalik@gmail.com>
2020-10-04 22:10:34 +0300

    secondary text field

--- worddeposit/Extensions/UITextField.swift
+++ worddeposit/Extensions/UITextField.swift
@@ -1,8 +1,5 @@
-import UIKit.UITextField
+import UIKit
 
 extension UITextField {
-    func validatedText(validationType: ValidatorType) throws -> String {
-        let validator = ValidatorFactory.validatorFor(type: validationType)
-        return try validator.validated(self.text!)
-    }
+    
 }
