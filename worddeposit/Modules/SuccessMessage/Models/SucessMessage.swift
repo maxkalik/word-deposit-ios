78b650832e3ba7f53c3e8c284f5ec2b0216f8e1b
maxkalik <maxkalik@gmail.com>
2021-06-27 20:21:14 +0300

    moving success message module

---
+++ worddeposit/Modules/SuccessMessage/Models/SucessMessage.swift
@@ -0,0 +1,13 @@
+//
+//  SucessMessage.swift
+//  worddeposit
+//
+//  Created by Maksim Kalik on 6/27/21.
+//  Copyright © 2021 Maksim Kalik. All rights reserved.
+//
+
+import Foundation
+
+struct SucessMessage {
+    
+}
